96b9d67665fde1b4a34f72f03e005ea34aca3a56
Jetstreck <mellinonauval@gmail.com>
2026-09-09 18:39:02 +0700

    feat:database

---
+++ confiq/database.go
@@ -0,0 +1,31 @@
+package confiq
+
+import (
+	"database/sql"
+	"fmt"
+	"os"
+
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/joho/godotenv"
+)
+
+func ConnectDB() (*sql.DB, error) {
+	_ = godotenv.Load()
+
+	dsn := os.Getenv("DB_URL")
+	if dsn == "" {
+		return nil, fmt.Errorf("DB_URL tidak ditemukan di .env")
+	}
+
+	db, err := sql.Open("mysql", dsn)
+	if err != nil {
+		return nil, fmt.Errorf("gagal membuka driver: %w", err)
+	}
+
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("gagal koneksi ke database: %w", err)
+	}
+
+	fmt.Println("Berhasil terhubung ke database MySQL (game_store)!")
+	return db, nil
+}
