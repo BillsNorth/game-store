7db87a4d967b942d1e2c5aadd3a83f6bd347f3f5
Jetstreck <mellinonauval@gmail.com>
2026-09-10 15:11:03 +0700

    add:entity cary

--- entity/models.go
+++ entity/models.go
@@ -38,16 +38,29 @@ type GameKey struct {
 	Status     string
 }
 
+type Cart struct {
+	ID        int
+	UserID    int
+	GameID    int
+	GameTitle string
+	Price     float64
+	Quantity  int
+	CreatedAt string
+}
+
 type Order struct {
 	ID          int
 	UserID      int
 	TotalAmount float64
-	CreatedAt   time.Time
+	CreatedAt   string
 }
 
 type OrderDetail struct {
-	ID              int
-	OrderID         int
-	GameKeyID       int
-	PriceAtPurchase float64
+	ID         int
+	OrderID    int
+	GameID     int
+	GameTitle  string
+	GameKeyID  int
+	LicenseKey string
+	Price      float64
 }
